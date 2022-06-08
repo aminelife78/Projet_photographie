8d1004a183adbabaff98f1432df960b891c05146
med amine <aminelife86@gmail.com>
2022-06-08 21:31:25 +0200

    save Data and favicon

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,44 +1,150 @@
 export const card_galerie = [
   {
-    image:"https://cdn.pixabay.com/photo/2016/03/14/14/21/bride-1255520_1280.jpg",
-    title:"Mariage"
-
+    image:
+      "https://cdn.pixabay.com/photo/2016/03/14/14/21/bride-1255520_1280.jpg",
+    title: "Mariage",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2016/03/27/21/35/woman-1284353_1280.jpg",
-    title:"Grossesse"
-
+    image:
+      "https://cdn.pixabay.com/photo/2016/03/27/21/35/woman-1284353_1280.jpg",
+    title: "Grossesse",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2016/11/19/14/28/bed-1839564_1280.jpg",
-    title:"Bébé"
-
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/19/14/28/bed-1839564_1280.jpg",
+    title: "Bébé",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2018/08/13/03/21/family-3602245_1280.jpg",
-    title:"Famille"
-
+    image:
+      "https://cdn.pixabay.com/photo/2018/08/13/03/21/family-3602245_1280.jpg",
+    title: "Famille",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2014/11/27/05/01/baptism-547222_1280.jpg",
-    title:"Baptême"
-
+    image:
+      "https://cdn.pixabay.com/photo/2014/11/27/05/01/baptism-547222_1280.jpg",
+    title: "Baptême",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2017/01/14/10/57/woman-1979272_1280.jpg",
-    title:"Couple"
-
+    image:
+      "https://cdn.pixabay.com/photo/2017/01/14/10/57/woman-1979272_1280.jpg",
+    title: "Couple",
   },
   {
-    image:"https://cdn.pixabay.com/photo/2017/01/23/19/40/woman-2003647_1280.jpg",
-    title:"Portrait"
-
+    image:
+      "https://cdn.pixabay.com/photo/2017/01/23/19/40/woman-2003647_1280.jpg",
+    title: "Portrait",
   },
-]
-
-
+];
 
 // mes images
+
+export const allImages = [
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+  [
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+    "../images/mariage/bride-1255520_1280.jpg",
+  ],
+];
+
+// Tarifs component
+
+export const card_Tarifs = [
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/10/06/03/30/man-1718099_1280.jpg",
+    title: "Juste moi",
+    text: "Séance pour une personne, en extérieur ou en studio",
+    prix: "130 euros",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
+    title: "Pour deux",
+    text: "Pour deux personnes, en extérieur ou en studio",
+    prix: "195 euros",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/04/07/23/20/father-2212101_1280.jpg",
+    title: "Famille",
+    text: "Pour la famille ou les amis jusqu’à 4 personnes, en extérieur ou en studio 30 euros en supplément par personne au-delà de 4 (hormis enfant jusqu’à 2 ans)",
+    prix: "210 euros",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/12/24/15/42/woman-5857744_1280.jpg",
+    title: "Il était une fois ",
+    text: "Photo de grossesse (À votre domicile, en extérieur ou en studio)",
+    prix: "160 euros",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/19/14/28/bed-1839564_1280.jpg",
+    title: "Mon bébé ",
+    text: "Photo d’enfant jusqu’à 3 ans (photo à domicile)",
+    prix: "100 euros",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2014/11/27/05/01/baptism-547222_1280.jpg",
+    title: "J’immortalise l’événement",
+    text: "sur mesure prestation de mariage ou baptême sur devis",
+    prix: "Sur devis",
+  },
+];
+
+/* mes images
 
 export const allImages = [
   
@@ -101,55 +207,4 @@ export const allImages = [
 
  
 
-]
- 
-
-// Tarifs component
-
-export const card_Tarifs = [
-  {
-    image:"https://cdn.pixabay.com/photo/2016/10/06/03/30/man-1718099_1280.jpg",
-    title:"Juste moi",
-    text : "Séance pour une personne, en extérieur ou en studio",
-    prix : "130 euros"
-
-  },
-  {
-    image:"https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
-    title:"Pour deux",
-    text: "Pour deux personnes, en extérieur ou en studio",
-    prix: "195 euros"
-
-
-
-  },
-  {
-    image:"https://cdn.pixabay.com/photo/2017/04/07/23/20/father-2212101_1280.jpg",
-    title:"Famille",
-    text: "Pour la famille ou les amis jusqu’à 4 personnes, en extérieur ou en studio 30 euros en supplément par personne au-delà de 4 (hormis enfant jusqu’à 2 ans)",
-    prix: "210 euros"
-
-  },
-  {
-    image:"https://cdn.pixabay.com/photo/2020/12/24/15/42/woman-5857744_1280.jpg",
-    title:"Il était une fois ",
-    text: "Photo de grossesse (À votre domicile, en extérieur ou en studio)",
-    prix: "160 euros"
-
-  },
-  {
-    image:"https://cdn.pixabay.com/photo/2016/11/19/14/28/bed-1839564_1280.jpg",
-    title:"Mon bébé ",
-    text: "Photo d’enfant jusqu’à 3 ans (photo à domicile)",
-    prix: "100 euros"
-
-  },
-  {
-    image:"https://cdn.pixabay.com/photo/2014/11/27/05/01/baptism-547222_1280.jpg",
-    title:"J’immortalise l’événement",
-    text: "sur mesure prestation de mariage ou baptême sur devis",
-    prix: "Sur devis"
-
-  },
- 
-]
+] */
