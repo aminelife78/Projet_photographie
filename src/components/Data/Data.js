d0f01fe24e826071db5daf96bf4ead79def8c8e2
med amine <aminelife86@gmail.com>
2022-06-13 11:56:36 +0200

    save fetch data in Photo and Tarifs

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -2,100 +2,42 @@ export const card_galerie = [
   {
     image:
       "https://cdn.pixabay.com/photo/2016/03/14/14/21/bride-1255520_1280.jpg",
-    title: "Mariage",
+    title: "Mariages",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2016/03/27/21/35/woman-1284353_1280.jpg",
-    title: "Grossesse",
+    title: "Grossesses",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2016/11/19/14/28/bed-1839564_1280.jpg",
-    title: "Bébé",
+    title: "Bebes",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2018/08/13/03/21/family-3602245_1280.jpg",
-    title: "Famille",
+    title: "Familles",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2014/11/27/05/01/baptism-547222_1280.jpg",
-    title: "Baptême",
+    title: "Baptemes",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2017/01/14/10/57/woman-1979272_1280.jpg",
-    title: "Couple",
+    title: "Couples",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2017/01/23/19/40/woman-2003647_1280.jpg",
-    title: "Portrait",
+    title: "Portraits",
   },
 ];
 
-// mes images
 
-export const allImages = [
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-  [
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-    "../images/mariage/bride-1255520_1280.jpg",
-  ],
-];
+
 
 // Tarifs component
 
@@ -208,3 +150,10 @@ export const allImages = [
  
 
 ] */
+
+
+
+
+
+
+
